71aec097ba0143ce9605785723e2819066d51051
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-19 12:34:36 +0530

    [Harini]: uc1 to start the position zero

---
+++ uc1snakeladder/uc1snakeladder/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+uc1snakeladder.uc1snakeladder sc = new uc1snakeladder.uc1snakeladder();
+sc.Position();
---
+++ uc1snakeladder/uc1snakeladder/uc1snakeladder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uc1snakeladder
+{
+    internal class uc1snakeladder
+    {
+        int position1 = 0, position2 = 0, count = 0;
+        public void Position()
+        {
+            Console.WriteLine("Initial  position of player is " + position2);
+
+        }
+
+
+
+
+
+    }
+}
