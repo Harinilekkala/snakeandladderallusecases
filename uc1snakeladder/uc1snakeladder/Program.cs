44e866b51b2dc36a81115c4adc3fb591c962b461
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-19 22:05:49 +0530

    [Harini]: usecase7 final report

--- uc1snakeladder/uc1snakeladder/Program.cs
+++ uc1snakeladder/uc1snakeladder/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-uc1snakeladder.uc1snakeladder sc = new uc1snakeladder.uc1snakeladder();
-sc.Position();
-int dice=sc.diceroll();
-sc.checkoption( dice);
-sc.dicerollgame( dice);
+SnakeLadder.Play sc = new SnakeLadder.Play();
+sc.Play();
+Play.rollDice();
+
+
+
--- uc1snakeladder/uc1snakeladder/uc1snakeladder.cs
+++ uc1snakeladder/uc1snakeladder/uc1snakeladder.cs
@@ -4,99 +4,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace uc1snakeladder
+namespace SnakeLadder
 {
-    internal class uc1snakeladder
+    internal class Play
     {
-        int position1 = 0, position2 = 0, count = 0, var=0;
-        public void Position()
-        {
-            Console.WriteLine("Initial  position of player is " + position2);
-
-        }
-        Random random = new Random();
-        public int diceroll()
-        {
-            int diceno = random.Next(1, 6);
-            Console.WriteLine("dice value of player is " + diceno);
-            return diceno;
-        }
-        public  void checkoption(int dice)
-            
-            
-        {
-            
-            switch (random.Next(0, 3))
-            {
-                case 0:
-                    position2 += 0;
-                    Console.WriteLine("Player got noPlay Option");
-                    break;
-                case 1:
-                    position2 -= dice;
-                    break;
-                case 2:
-                    position2 += dice;
-                    break;
-                default:
-                    break;
-            }
-            Console.WriteLine("dice value of player is{0} with position at {1} ", dice, position2);
-
-        }
-         
         
-        public void dicerollgame(int dice)
+        int position1 = 0, position2 = 0, count = 0;
+        int Player1, Player2;
+        public void rollDice()
         {
-            while (position2 < 100)
+
+            Random random = new Random();
+            while (position1 != 100 && position2 != 100)
             {
-                switch (random.Next(0, 3))
+                Player1 = random.Next(0, 7);
+                Player2 = random.Next(0, 7);
+                int check1 = random.Next(0, 3);
+                int check2 = random.Next(0, 3);
+                count++;
+
+                switch (check1)
                 {
                     case 0:
-                        position2 += 0;
-                        Console.WriteLine("Player got noPlay Option");
                         break;
                     case 1:
-                        position2 -= dice;
+                        position1 += Player1;
                         break;
                     case 2:
-                        position2 += dice;
-                        break;
-                    default:
+                        position1 -= Player1;
                         break;
                 }
+
+                if (position1 < 0)
+                    position1 = 0;
+                if (position1 > 100)
+                    position1 -= Player1;
+
+                switch (check2)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        position2 += Player2;
+                        break;
+                    case 2:
+                        position2 -= Player2;
+                        break;
+                }
+
                 if (position2 < 0)
-                {
                     position2 = 0;
-                }
                 if (position2 > 100)
-                {
-                    position2 -= dice;
-                    while (position2 < 100)
-                    {
-                        if (dice == (100 - position2))
-                        {
-                            position2 = 100;
-                            Console.WriteLine("Player Won the Game");
-                        }
-                        else
-                        {
-                            continue;
-                        }
-                    }
-                }
-                Console.WriteLine("Player present Position is at {0}\n", position2);
-                Console.WriteLine("No of times die was played to win the game");
+                    position2 -= Player2;
 
-
+                Console.WriteLine("Dice roll: " + count + " Position of Player 1: " + position1);
+                Console.WriteLine("Dice roll: " + count + " Position of Player 2: " + position2);
             }
-            Console.WriteLine("Player rolls the dice {0} time to reach Position {1}\n ", var, position2);
-            
+            if (position1 == 100)
+                Console.WriteLine("Player 1 won the match.");
+            else
+                Console.WriteLine("Player 2 won the match.");
+            Console.WriteLine("Dice rolled for " + count + " no. of time.");
+
         }
     }
 }
 
 
 
-    
 
